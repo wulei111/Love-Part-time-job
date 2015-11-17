70eb034d6ff45949d94e290743ab72aa8b6280ca
Emperores <1443957775@qq.com>
2015-11-17 10:36:31 +0800

    add LoveJobs

--- LoveJob/app/src/main/java/com/best/lovejob/LoginActivity.java
+++ LoveJob/app/src/main/java/com/best/lovejob/LoginActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- LoveJobs/app/src/main/java/com/best/lovejobs/MainActivity.java
+++ LoveJobs/app/src/main/java/com/best/lovejobs/MainActivity.java
@@ -1,10 +1,11 @@
-package com.best.lovejob;
+package com.best.lovejobs;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
